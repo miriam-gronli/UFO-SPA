6b473c3cf708ae83a9d3301e444723e36ce24709
miriam-gronli <miriam.gronli@gmail.com>
2022-11-19 12:01:59 +0100

    Første oppsett av testing

---
+++ UFO-Test/UnitTest1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading.Tasks;
+using KundeApp2.Controllers;
+using KundeApp2.DAL;
+using KundeApp2.Model;
+using Moq;
+using Xunit;
+
+namespace UFO_Test;
+
+public class UnitTest1
+{
+    [Fact]
+    public async Task Lagre()
+    {
+        //Arrange
+        var innObservasjon = new Observasjon
+        {
+            Id = 1,
+            Navn = "Test Case",
+            Postkode = "0582",
+            Beskrivelse = "OMG, det var en UFO!",
+            Dato = "19.07.21",
+            Tid = "Midnight",
+        };
+
+        var mock = new Mock<IObservasjonRepository>();
+        mock.Setup(o => o.Lagre(innObservasjon)).ReturnsAsync(true);
+        var observasjonController = new ObservasjonController(mock.Object);
+
+    }
+}
+
