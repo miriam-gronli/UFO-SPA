864abf1f2c744a5654b46011874504fbd2261f1c
Nora-bakke <71878743+Nora-bakke@users.noreply.github.com>
2022-11-20 16:58:47 +0100

    Update DBInit.cs
    La til en admin-bruker i "DBInit"

--- UFO-SPA/DAL/DBInit.cs
+++ UFO-SPA/DAL/DBInit.cs
@@ -5,44 +5,58 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace KundeApp2.DAL
+namespace EksamenVersjon3.DAL
 {
-    public static class DBInit
+    //Denne klassen er hentet fra KundeApp2-med-DB filen i KundeApp2-med-DAL mappen fra canvas
+    public static class DBInit //Klasse som initierer databasen, og legger til dummy data i databasen
     {
-        public static void Seed(IApplicationBuilder app)
+        public static void Initialize(IApplicationBuilder app)
         {
-            var serviceScope = app.ApplicationServices.CreateScope();
-            
-            var db = serviceScope.ServiceProvider.GetService<ObservasjonContext>();
-
-            // må slette og opprette databasen hver gang når den skal initieres (seed`es)
-            db.Database.EnsureDeleted();
-            db.Database.EnsureCreated();
-
-            var observasjon1 = new Observasjoner //Dummy data #1
+            using (var serviceScope = app.ApplicationServices.CreateScope())
             {
-                Id = 1,
-                Postkode = "0001",
-                Beskrivelse = "Grønn UFO",
-                Navn = "Ola Nordmann",
-                Dato = "1 januar 2022",
-                Tid = "22:30"
-            };
+                var context = serviceScope.ServiceProvider.GetService<ObservasjonContext>();
 
-            var observasjon2 = new Observasjoner //Dummy data #2
-            {
-                Id = 2,
-                Postkode = "0002",
-                Beskrivelse = "Blå UFO",
-                Navn = "Sam Møller",
-                Dato = "2 januar 2022",
-                Tid = "20:00"
-            };
-            //Legger til dummy dataen i databasen
-            db.Observasjoner.Add(observasjon1);
-            db.Observasjoner.Add(observasjon2);
+                // Disse forsikrer at databasen slettes og opprettes hver gang den skal initieres
+                context.Database.EnsureDeleted();
+                context.Database.EnsureCreated();
 
-            db.SaveChanges();
+                var observasjon1 = new Observasjoner //Dummy data #1
+                {
+                    Id = 1,
+                    Postkode = "0001",
+                    Beskrivelse = "Gr�nn UFO",
+                    Navn = "Ola Nordmann",
+                    Dato = "1 januar 2022",
+                    Tid = "22:30"
+                };
+
+                var observasjon2 = new Observasjoner //Dummy data #2
+                {
+                    Id = 2,
+                    Postkode = "0002",
+                    Beskrivelse = "Bl� UFO",
+                    Navn = "Sam M�ller",
+                    Dato = "2 januar 2022",
+                    Tid = "20:00"
+                };
+
+                // lag en påoggingsbruker
+                //Koden under er hentet fra "DAL" mappen som igjen ligger under mappen "KundeApp2-med-hash-logginn" hentet fra canvas
+                var bruker = new Brukere();
+                bruker.Brukernavn = "Admin";
+                var passord = "Test11";
+                byte[] salt = ObservasjonRepository.LagSalt();
+                byte[] hash = ObservasjonRepository.LagHash(passord, salt);
+                bruker.Passord = hash;
+                bruker.Salt = salt;
+
+                //Legger til dummy dataen i databasen
+                context.Observasjoner.Add(observasjon1);
+                context.Observasjoner.Add(observasjon2);
+                context.Brukere.Add(bruker);
+
+                context.SaveChanges();
+            }
         }
-    }  
+    }
 }
