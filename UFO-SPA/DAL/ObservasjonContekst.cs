4e400458d4064d3ff9e43015173edaec8986b638
Nora-bakke <71878743+Nora-bakke@users.noreply.github.com>
2022-11-20 17:03:13 +0100

    Revert "Update ObservasjonContekst.cs"
    This reverts commit 37b2f3a09d4fad0b5e67b892040b5b85f041ade8.

--- UFO-SPA/DAL/ObservasjonContekst.cs
+++ UFO-SPA/DAL/ObservasjonContekst.cs
@@ -1,10 +1,10 @@
-﻿using Microsoft.EntityFrameworkCore;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
 
-namespace EksamenVersjon3.DAL
+namespace KundeApp2.DAL
 {
     //Denne klassen er hentet fra KundeApp2-med-DB filen i KundeApp2-med-DAL mappen fra canvas
     public class Observasjoner //Denne klassen blir referert til dersom man oppretter data som skal bli lagt til i databasen
@@ -17,15 +17,6 @@ namespace EksamenVersjon3.DAL
         public string Tid { get; set; }
     }
 
-    //Denne koden er hentet fra "DAL" mappen som igjen ligger under mappen "KundeApp2-med-hash-logginn" hentet fra canvas
-    public class Brukere
-    {
-        public int Id { get; set; }
-        public string Brukernavn { get; set; }
-        public byte[] Passord { get; set; }
-        public byte[] Salt { get; set; }
-    }
-
 
     public class ObservasjonContext : DbContext //Klasse som oppretter databasen fysisk dersom databasen ikke er opprettet
     {
@@ -36,8 +27,6 @@ namespace EksamenVersjon3.DAL
         }
 
         public DbSet<Observasjoner> Observasjoner { get; set; }
-        public DbSet<Brukere> Brukere { get; set; }
-
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
