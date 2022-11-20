51200754cfc115774fef1d49ddd25d9b0eeeb7d7
Nora-bakke <71878743+Nora-bakke@users.noreply.github.com>
2022-11-20 17:17:05 +0100

    Update IObservasjonRepository.cs

--- UFO-SPA/DAL/IObservasjonRepository.cs
+++ UFO-SPA/DAL/IObservasjonRepository.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using EksamenVersjon3.Model;
 using KundeApp2.Model;
 
 namespace KundeApp2.DAL
@@ -12,5 +13,6 @@ namespace KundeApp2.DAL
         Task<bool> Slett(int id);
         Task<Observasjon> HentEn(int id);
         Task<bool> Endre(Observasjon endreObservasjon);
+        Task<bool> LoggInn(Bruker bruker);
     }
 }
