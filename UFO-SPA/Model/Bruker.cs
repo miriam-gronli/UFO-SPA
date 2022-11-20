1495c73e5f7f04c7e946b0d783ee759ea64f3338
Nora-bakke <71878743+Nora-bakke@users.noreply.github.com>
2022-11-20 16:51:26 +0100

    Create Bruker.cs
    Laget Bruker.cs under "Model" mappen

---
+++ UFO-SPA/Model/Bruker.cs
@@ -0,0 +1,16 @@
+﻿//Denne koden er hentet fra "Model" mappen som igjen ligger under mappen "KundeApp2-med-hash-logginn" hentet fra canvas
+
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace EksamenVersjon3.Model
+{
+    public class Bruker
+    {
+        [RegularExpression(@"^[a-zA-ZæøåÆØÅ. \-]{2,20}$")]
+        public String Brukernavn { get; set; }
+        [RegularExpression(@"^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{6,}$")]
+        public String Passord { get; set; }
+    }
+}
+
